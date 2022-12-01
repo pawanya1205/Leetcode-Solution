7f1460f42aab40fc5fbb86178d8d2df4befdf12f
Pawan Kumar Yadav <pawanya1205@gmail.com>
2022-12-01 19:22:28 +0530

    Time: 4 ms (73.06%), Space: 6.6 MB (56.35%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     bool halvesAreAlike(string s) {
-                int n=s.length();
-        int cnt1=0,cnt2=0;
-       for(int i=0;i<n/2;i++){
-           if(s[i]=='a'||s[i]=='A'||s[i]=='e'||s[i]=='E'||s[i]=='i'||s[i]=='I'||s[i]=='o'||s[i]=='O'||s[i]=='u'||s[i]=='U')
-               cnt1++;
-       }
-    for(int i=n/2;i<n;i++){
-        if(s[i]=='a'||s[i]=='A'||s[i]=='e'||s[i]=='E'||s[i]=='i'||s[i]=='I'||s[i]=='o'||s[i]=='O'||s[i]=='u'||s[i]=='U')
-            cnt2++;
-    }
-   return cnt1==cnt2;
+        int a=0,b=0;
+        for(int i=0;i<(s.length())/2;i++){
+                            if(s[i]=='a'||s[i]=='A'||s[i]=='e'||s[i]=='E'||s[i]=='i'||s[i]=='I'||s[i]=='o'||s[i]=='O'||s[i]=='u'||s[i]=='U')
+                a++;
+            
+        }
+        for(int i=(s.length()/2);i<s.length();i++){
+             if(s[i]=='a'||s[i]=='A'||s[i]=='e'||s[i]=='E'||s[i]=='i'||s[i]=='I'||s[i]=='o'||s[i]=='O'||s[i]=='u'||s[i]=='U')
+                 b++;
+        }
+       return a==b;
+        
+        
     }
 };
